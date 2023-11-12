e52473798274b07d5c4042ea69136fed22686d97
robert <robert@arles.us>
2023-11-12 18:03:47 +0000

    working debug output. why so hard?

--- assistant.py
+++ assistant.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import json
+import pprint
 
 from openai import OpenAI
 
@@ -10,6 +11,5 @@ assistantID = "asst_vVjkIX5UzTyGROBG1fj6Lmkz"
 threadID = "thread_ufYt3oNUn8dnqAP4lobD5zZp"
 
 my_assistant = client.beta.assistants.retrieve(assistantID)
-# Debug. Colored and formatted print of my_assistant
-# pretty print the assistant
-print(json.dumps(my_assistant.model_dump_json(), indent=4, sort_keys=True))
+
+print(pprint.pformat(my_assistant.model_dump()))
